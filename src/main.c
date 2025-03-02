ce2463d0a7fc58520b1e54071a13e208d9262a2f
drew <drewbensinger@gmail.com>
2025-03-02 10:48:19 -0500

    Draw initial window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,63 @@
 #include <SDL3/SDL.h>
-#include <stdint.h>
-#include <stdio.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
-  printf("hi\n");
-  SDL_Log("hi from sdl\n");
+const int SCREEN_WIDTH = 680;
+const int SCREEN_HEIGHT = 480;
+
+void shutdown_sdl()
+{
+}
+
+int main(int argc, char* args[])
+{
+  SDL_Window* window = NULL;
+  SDL_Surface* screen_surface = NULL;
+
+  if (!SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_Log("SDL couldn't initialize. SDL_Error: %s\n", SDL_GetError());
+  }
+
+  window = SDL_CreateWindow("pong", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  if (!window)
+  {
+    SDL_Log("Can't create window. SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  screen_surface = SDL_GetWindowSurface(window);
+  SDL_FillSurfaceRect(screen_surface, NULL, SDL_MapSurfaceRGB(screen_surface, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+
+  // game loop
+
+  bool quit = false;
+  while (!quit)
+  {
+    SDL_Event e;
+    while (SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_EVENT_QUIT)
+      {
+        quit = true;
+      }
+      else if (e.type == SDL_EVENT_KEY_DOWN)
+      {
+        switch (e.key.key)
+        {
+        case SDLK_ESCAPE:
+          quit = true;
+          break;
+        default:
+          break;
+        }
+      }
+    }
+  }
+
+  // shutdown
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
