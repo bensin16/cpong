2644af75002dd62bdd7e6f5110405dfbc35b6a54
drew <drewbensinger@gmail.com>
2025-03-04 22:36:14 -0500

    Draw ball image

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
-#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_error.h"
+#include "SDL3/SDL_log.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 #include <stdint.h>
 #include <time.h>
 
@@ -15,6 +17,8 @@ const uint16_t INITIAL_P1_PADDLE_Y = SCREEN_HEIGHT / 2;
 const uint16_t INITIAL_P2_PADDLE_X = SCREEN_WIDTH - 10 - PADDLE_WIDTH;
 const uint16_t INITIAL_P2_PADDLE_Y = SCREEN_HEIGHT / 2;
 
+const char* BALL_PATH = "images/pongball.png";
+
 typedef struct
 {
   uint16_t x_position;
@@ -23,6 +27,25 @@ typedef struct
   uint16_t width;
   int8_t vertical_velocity;
 } Paddle;
+
+SDL_Texture* load_texture(const char* path, SDL_Renderer* renderer)
+{
+  SDL_Surface* loaded_surface = IMG_Load(path);
+  if (!loaded_surface)
+  {
+    SDL_Log("Unable to load image %s. SDL_image Error: %s\n", path, SDL_GetError());
+    return NULL;
+  }
+
+  SDL_Texture* new_texture = SDL_CreateTextureFromSurface(renderer, loaded_surface);
+  if (!new_texture)
+  {
+    SDL_Log("Unable to create texture from %s. SDL Error: %s\n", path, SDL_GetError());
+  }
+
+  SDL_DestroySurface(loaded_surface);
+  return new_texture;
+}
 
 int main(int argc, char* args[])
 {
@@ -42,6 +65,13 @@ int main(int argc, char* args[])
   }
 
   SDL_SetRenderVSync(renderer, 1);
+
+  SDL_Texture* ball_texture = load_texture(BALL_PATH, renderer);
+  if (!ball_texture)
+  {
+    SDL_Log("Failed to load ball texture\n");
+    return 1;
+  }
 
   screen_surface = SDL_GetWindowSurface(window);
   SDL_FillSurfaceRect(screen_surface, NULL, SDL_MapSurfaceRGB(screen_surface, 0x11, 0x11, 0x11));
@@ -63,7 +93,7 @@ int main(int argc, char* args[])
     0,
   };
 
-  SDL_FRect windowRect = {
+  SDL_FRect window_rect = {
     0,
     0,
     SCREEN_WIDTH,
@@ -133,17 +163,22 @@ int main(int argc, char* args[])
     // render
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0x11, 0x11, 0x11, 0xFF);
-    SDL_RenderFillRect(renderer, &windowRect);
+    SDL_RenderFillRect(renderer, &window_rect);
 
-    SDL_FRect p1Rect = { p1.x_position, p1.y_position, p1.width, p1.height };
-    SDL_FRect p2Rect = { p2.x_position, p2.y_position, p2.width, p2.height };
+    SDL_FRect p1_rect = { p1.x_position, p1.y_position, p1.width, p1.height };
+    SDL_FRect p2_rect = { p2.x_position, p2.y_position, p2.width, p2.height };
 
-    SDL_FRect rects[] = { p1Rect, p2Rect };
+    SDL_FRect rects[] = { p1_rect, p2_rect };
 
     SDL_SetRenderDrawColor(renderer, 0xEE, 0xEE, 0xEE, 0xFF);
     SDL_RenderFillRects(renderer, rects, 2);
-    SDL_RenderFillRect(renderer, &p1Rect);
-    SDL_RenderFillRect(renderer, &p2Rect);
+    SDL_RenderFillRect(renderer, &p1_rect);
+    SDL_RenderFillRect(renderer, &p2_rect);
+
+    SDL_FRect ball_rect = { (float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, 16, 16 };
+
+    SDL_RenderTexture(renderer, ball_texture, NULL, &ball_rect);
+
     SDL_RenderPresent(renderer);
   }
 
