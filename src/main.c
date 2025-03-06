4211e44e09fa8cb019e7d1f8fbf78e7c708204c6
drew <drewbensinger@gmail.com>
2025-03-05 19:59:31 -0500

    Ball wall collision

--- src/main.c
+++ src/main.c
@@ -17,6 +17,13 @@ const uint16_t INITIAL_P1_PADDLE_Y = SCREEN_HEIGHT / 2;
 const uint16_t INITIAL_P2_PADDLE_X = SCREEN_WIDTH - 10 - PADDLE_WIDTH;
 const uint16_t INITIAL_P2_PADDLE_Y = SCREEN_HEIGHT / 2;
 
+const uint16_t INITIAL_BALL_X_POSITION = SCREEN_WIDTH / 2;
+const uint16_t INITIAL_BALL_Y_POSITION = SCREEN_HEIGHT / 2;
+const uint16_t INITIAL_BALL_HEIGHT = 16;
+const uint16_t INITIAL_BALL_WIDTH = 16;
+const int8_t INITIAL_BALL_X_VELOCITY = -2;
+const int8_t INITIAL_BALL_Y_VELOCITY = -1;
+
 const char* BALL_PATH = "images/pongball.png";
 
 typedef struct
@@ -27,6 +34,16 @@ typedef struct
   uint16_t width;
   int8_t vertical_velocity;
 } Paddle;
+
+typedef struct
+{
+  uint16_t x;
+  uint16_t y;
+  uint16_t height;
+  uint16_t width;
+  int8_t x_velocity;
+  int8_t y_velocity;
+} Ball;
 
 SDL_Texture* load_texture(const char* path, SDL_Renderer* renderer)
 {
@@ -93,6 +110,15 @@ int main(int argc, char* args[])
     0,
   };
 
+  Ball ball = {
+    INITIAL_BALL_X_POSITION,
+    INITIAL_BALL_Y_POSITION,
+    INITIAL_BALL_HEIGHT,
+    INITIAL_BALL_WIDTH,
+    INITIAL_BALL_X_VELOCITY,
+    INITIAL_BALL_Y_VELOCITY,
+  };
+
   SDL_FRect window_rect = {
     0,
     0,
@@ -153,12 +179,24 @@ int main(int argc, char* args[])
       }
     }
 
+    if (ball.x <= 0 || ball.x + ball.width >= SCREEN_WIDTH)
+    {
+      ball.x_velocity = -ball.x_velocity;
+    }
+    if (ball.y <= 0 || ball.y + ball.height >= SCREEN_HEIGHT)
+    {
+      ball.y_velocity = -ball.y_velocity;
+    }
+
     // logic
     if (p1.y_position + p1.vertical_velocity + PADDLE_HEIGHT < SCREEN_HEIGHT && p1.y_position + p1.vertical_velocity > 0)
       p1.y_position += p1.vertical_velocity;
 
     if (p2.y_position + p2.vertical_velocity + PADDLE_HEIGHT < SCREEN_HEIGHT && p2.y_position + p2.vertical_velocity > 0)
       p2.y_position += p2.vertical_velocity;
+
+    ball.x += ball.x_velocity;
+    ball.y += ball.y_velocity;
 
     // render
     SDL_RenderClear(renderer);
@@ -169,14 +207,10 @@ int main(int argc, char* args[])
     SDL_FRect p2_rect = { p2.x_position, p2.y_position, p2.width, p2.height };
 
     SDL_FRect rects[] = { p1_rect, p2_rect };
-
     SDL_SetRenderDrawColor(renderer, 0xEE, 0xEE, 0xEE, 0xFF);
     SDL_RenderFillRects(renderer, rects, 2);
-    SDL_RenderFillRect(renderer, &p1_rect);
-    SDL_RenderFillRect(renderer, &p2_rect);
 
-    SDL_FRect ball_rect = { (float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, 16, 16 };
-
+    SDL_FRect ball_rect = { ball.x, ball.y, ball.width, ball.height };
     SDL_RenderTexture(renderer, ball_texture, NULL, &ball_rect);
 
     SDL_RenderPresent(renderer);
